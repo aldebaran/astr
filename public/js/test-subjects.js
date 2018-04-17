33c4c357a20b39c0018158d8a980703843a98387
guillaume.fradet <guillaume.fradet@softbankrobotics.com>
2018-04-17 16:57:26 +0200

    created test subjects page

---
+++ public/js/test-subjects.js
@@ -0,0 +1,8 @@
+(function($) {
+  "use strict";
+
+  $('#buttonMoreConfig').click(function(){
+    $('#formConfig').append('<input type="text" class="form-control config" id="inputConfig" placeholder="Enter another configuration" style="margin-top:1em;">')
+  })
+
+})(jQuery);
