16106203c559dc35b86727cbddad8ee734a0268c
guillaume.fradet <guillaume.fradet@softbankrobotics.com>
2018-04-18 14:46:40 +0200

    test subjects on website (Issues #7 #42)

--- public/js/test-subjects.js
+++ public/js/test-subjects.js
@@ -1,31 +1,83 @@
 (function($) {
   "use strict";
 
+  // Create a new test subject
   var nbConfig = 1;
-
   $('#buttonMoreConfig').click(function(){
+    // check if the last configuration row is not empty
     if($('#inputConfig'+nbConfig).val().trim() !== '') {
       nbConfig++;
       $('#formConfig').append('' +
       '<div class="row config">' +
         '<div class="col">' +
-          '<input type="text" class="form-control" id="inputConfig' + nbConfig + '" placeholder="Enter another configuration">' +
+          '<input type="text" class="form-control inputConfig" id="inputConfig' + nbConfig + '" placeholder="Enter another configuration">' +
         '</div>' +
         '<div class="col">' +
-          '<input class="form-control" id="configName' + nbConfig + '" type="text" placeholder="" disabled>' +
+          '<input class="form-control configName" id="configName' + nbConfig + '" type="text" placeholder="" disabled>' +
         '</div>' +
       '</div>');
-      $('#inputConfig'+nbConfig).change(function(){
-        $('#configName'+nbConfig).attr('placeholder', $('#inputConfig'+nbConfig).val().trim().toLowerCase().replace(/\s+/g, '_'));
-      });
     } else {
       alert('Fulfill the actual configuration input to add another!')
     }
   });
 
-  $('#inputConfig1').change(function(){
-    $('#configName1').attr('placeholder', $('#inputConfig1').val().trim().toLowerCase().replace(/\s+/g, '_'));
+  // modify the configuration name
+  $('#submitNewSubject').on('change', '.inputConfig', function(){
+    $(this).parent().parent().find('.configName').attr('placeholder', $(this).val().trim().toLowerCase().replace(/\s+/g, '_'));
   });
 
+  $('#submitNewSubject').submit(function(e){
+    e.preventDefault();
+    $.get('api/user/profile', function(user){
+      // check if the user is logged
+      if(user.name) {
+        var subject = {
+          name: $('#inputName').val().trim().replace(/\s+/g, ' '),
+          configuration: [],
+          author: user.name,
+        }
+        $('.configName').each(function(){
+          if($(this).attr('placeholder') !== ''){
+            subject.configuration.push($(this).attr('placeholder'));
+          }
+        })
+
+        // send the new subject
+        $.post('api/test-subjects', subject, function(data){
+          alert(JSON.stringify(data, null, 2))
+          location.reload();
+        })
+
+      } else {
+        alert('Please login to add a new test subject !')
+      }
+    });
+  });
+
+
+  // Existing subjects
+  $.get('api/test-subjects', function(subjects){
+    subjects.forEach(function(subject){
+      $('#selectSubject').append('<option value="' + subject['_id'] + '">' + subject.name + '</option>');
+    })
+  })
+
+  $('#selectSubject').change(function(){
+    if($('#selectSubject').val() !== "default"){
+      $.get('api/test-subjects/' + $('#selectSubject').val(), function(data){
+        $('#infoSubject').html('' +
+        '<span class="key"> Name: </span><span class="value">' + data.name + '</span><br>' +
+        '<span class="key"> Author: </span><span class="value">' + data.author + '</span><br>' +
+        '<span class="key"> Configuration parameters:</span><br>');
+        data.configuration.forEach(function(config){
+          $('#infoSubject').append('<li><span class="value">' + config + '</span></li>');
+        })
+
+      })
+    } else {
+      $('#infoSubject').html('');
+    }
+
+  })
 
 })(jQuery);
