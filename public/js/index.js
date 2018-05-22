ba3bcc00ce33837ae8d5914dd14112db61b1fd0d
guillaume.fradet <guillaume.fradet@softbankrobotics.com>
2018-05-22 17:02:55 +0200

    (design): scroll to the top button (Issue #71)

--- public/js/index.js
+++ public/js/index.js
@@ -24,7 +24,7 @@
     e.preventDefault();
   });
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $('.content-wrapper').scroll(function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -37,7 +37,7 @@
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(event) {
     var $anchor = $(this);
-    $('html, body').stop().animate({
+    $('.content-wrapper').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top)
     }, 1000, 'easeInOutExpo');
     event.preventDefault();
