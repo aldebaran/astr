4d4216bdbaa0cbcd63654decd2b6ebe0541df7d8
guillaume.fradet <guillaume.fradet@softbankrobotics.com>
2018-05-23 13:26:36 +0200

    (design): improved the drop zone

--- public/js/dropzone.js
+++ public/js/dropzone.js
@@ -3577,5 +3577,21 @@
     }
   }
 
+  $(document).bind('dragover', function (e) {
+    var dropZone = $('#dropzone'),
+      timeout = window.dropZoneTimeout;
+    if (timeout) {
+      clearTimeout(timeout);
+    } else {
+      dropZone.addClass('in');
+    }
+    var hoveredDropZone = $(e.target).closest(dropZone);
+    dropZone.toggleClass('hover', hoveredDropZone.length);
+    window.dropZoneTimeout = setTimeout(function () {
+      window.dropZoneTimeout = null;
+      dropZone.removeClass('in hover');
+    }, 100);
+  });
+
 
 })(jQuery)
