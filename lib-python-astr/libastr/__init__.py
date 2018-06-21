eac0de428528d0b2abbdc9b0596de3cb2ecba30a
guillaume.fradet <guillaume.fradet@softbankrobotics.com>
2018-06-21 10:01:19 +0200

    (python lib): alert if not using python 3 (Issue #109)

--- lib-python-astr/libastr/__init__.py
+++ lib-python-astr/libastr/__init__.py
@@ -1,1 +1,6 @@
-from .resources import Astr
+import sys
+try:
+    from .resources import Astr
+except Exception:
+    print("Can't import the library, probably because your python is too old!")
+    print("You are running Python {}, try 3.5".format(sys.version_info[:2]))
